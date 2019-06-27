63ecf343863526de8e5be1a0b90e53dc5613df45
hankfrank <henryfrank1@yahoo.com>
2019-06-27 13:48:05 -0700

    Updating app.jsx to include comments to help me reference later.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  // your Javascript goes here
   constructor( props ) {
     super( props );
 
-    this.state = {
+    this.state = {  //these need to have their initial state set here or they are not able to have their state referenced anywhere later in the Component. 
       balance: "",
       rate: "",
       term: "",
       output: "",
-      result: null
+      result: null //needing to delare initial state of this here to be able to setState on it later. 
     }
+    //these functions need to be bound to the component or they are not accessable to any other part of the component. This is how you bind. 
     this.onChange = this.onChange.bind(this)
     this.calculate = this.calculate.bind(this)
     this.handleClick = this.handleClick.bind(this)
   }
- 
+ //A function that is making it possible to collect the value of any item in the HTML below that has it's onChange attr set below to onChange
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value })
   }
-
+//the calc function that is running the math. 
   calculate(balance, rate, term) {
-    balance = this.state.balance;
+    balance = this.state.balance; //if these are declared below in handle click they reset the state of the output ot the initial state for some reason. 
     rate = this.state.rate;
     term = this.state.term;
     
@@ -31,20 +31,21 @@ export default class App extends React.Component {
     const top = r * (1 + r) ** n;
     const bottom = (1 + r) ** n - 1;
 
-    var payment = (balance * (top / bottom)).toFixed(2);   //I think this is the right math? 
+    var payment = (balance * (top / bottom)).toFixed(2);
     return payment;
   }
+  //handle click here is taking the result of the calc function and outputting it below to the h3 element by setting it's state to be the result of themath. `` are template literals and are how it's being concat'd
   handleClick(e) {
     e.preventDefault();
 
-    var result = this.calculate();
+    var result = this.calculate();  //this is necessary here or it cant find calculate
     console.log(result);
 
-    this.setState({
+    this.setState({ //passing result down ot the h3 below
       output: `$${result} + "is your monthly payment."`
     });
   }
-
+//whats rendered, the above is working off of and onto this as though it were the DOM(withnone of the DOM functionality using this/state for all of that)
   render() {
     return (
       <div className='container'>
@@ -91,6 +92,7 @@ export default class App extends React.Component {
             </select>
             <button
               name="submit"
+              id="button"
               onClick={this.handleClick}
               >Submit</button>
           </div>
